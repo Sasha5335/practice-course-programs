19dc164ff0b2f0f3d382871a2c2f30377f701ada
Aleksandr <aleksandr@gmail.com>
2021-08-11 19:17:45 +0300

    Add Counter

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      varibleNum: null,
+      checkedRadioBtn: true,
+      countId: null,
+    };
+  }
+
+  varibleNumber = (event) => {
+    this.setState({
+      varibleNum: event.target.value,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      checkedRadioBtn: !this.state.checkedRadioBtn,
+    });
+  };
+
+  counterNumber = () => {
+    const { count, varibleNum, checkedRadioBtn } = this.state;
+    const number = Number(count);
+    const varibleNumber = Number(varibleNum);
+    this.setState({
+      count: checkedRadioBtn ? number + varibleNumber : number - varibleNumber,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <>
+        <h2>{count}</h2>
+        <button onClick={this.handleClick}>
+          {this.state.checkedRadioBtn ? '+' : '-'}
+        </button>
+        <br />
+        <input type="number" onChange={this.varibleNumber} />
+        <br />
+        <button onClick={this.counterNumber}>Добавить</button>
+        <br />
+        <br />
+        <button onClick={this.autoClicker}>autoClick </button>
+      </>
+    );
+  }
+}
+
+export default Counter;
